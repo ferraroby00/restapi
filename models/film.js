bab0fa49613e6e04bd0d6e745c8f8f04cee7b1f1
ferraroby00 <104848690+ferraroby00@users.noreply.github.com>
2022-05-03 17:32:14 +0200

    Add files via upload

---
+++ models/film.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const FilmSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    }
+});
+
+export default mongoose.model('Films', FilmSchema);
---
+++ models/rating.js
@@ -0,0 +1,19 @@
+import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
+
+const RatingSchema = mongoose.Schema({
+    ratedBy: {
+        type: ObjectId,
+        required: true
+    },
+    filmId: {
+        type: ObjectId,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    }
+});
+
+export default mongoose.model('Ratings', RatingSchema);
