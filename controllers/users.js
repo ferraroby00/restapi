a8618de794268fcc8f2939c0a3f4f10ec8dd0a93
Roberto Ferrareis <104848690+ferraroby00@users.noreply.github.com>
2022-05-28 17:37:48 +0200

    Fix

--- controllers/users.js
+++ controllers/users.js
@@ -24,6 +24,7 @@ function getSequenceNextValue(seqName) {
 
 //POST HANDLER - Inserts a new user document
 export const createUser = (req, res) => {
+  getSequenceNextValue("users");
   const user = new User({
     name: req.body.name,
     last: req.body.last,
