9d203c5d25a723b36db554431ed4d17015b4272b
Roberto Ferraries <104848690+ferraroby00@users.noreply.github.com>
2022-05-28 12:19:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import filmRoutes from './routes/films.js';
-import userRoutes from './routes/users.js';
-import ratingRoutes from './routes/ratings.js';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import filmRoutes from "./routes/films.js";
+import userRoutes from "./routes/users.js";
+import ratingRoutes from "./routes/ratings.js";
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.use('/films',filmRoutes);
+app.use("/films", filmRoutes);
 
-app.use('/users',userRoutes);
+app.use("/users", userRoutes);
 
-app.use('/ratings',ratingRoutes);
+app.use("/ratings", ratingRoutes);
 
 //Homepage
-app.get('/home',(req,res) => {
-    res.send('Welcome to the homepage');
+app.get("/home", (req, res) => {
+  res.send("Welcome to the homepage");
 });
 
 //Connection to database
-mongoose.connect('mongodb://127.0.0.1:27017/ufr');
+mongoose.connect(
+  "mongodb+srv://R_Ferrareis:Tesi22__@thesis.mvjviod.mongodb.net/test"
+);
 const con = mongoose.connection;
-con.on('open', () => {console.log('Connected to database');});
-con.on('error', () => {console.log('Error');});
+con.on("open", () => {
+  console.log("Connected to database");
+});
+con.on("error", () => {
+  console.log("Error");
+});
 
 //Server listening on port PORT
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
