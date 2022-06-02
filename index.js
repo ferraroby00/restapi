b8011b04eba0a1e3c807e0ca00da70aa46638d91
Roberto Ferrareis <104848690+ferraroby00@users.noreply.github.com>
2022-06-02 20:16:37 +0200

    Fixed parsing issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
-
+app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json());
 
 app.use("/movies", movieRoutes);
